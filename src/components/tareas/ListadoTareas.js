c5c67a552bbd066363a40b8ec630407e3c0f94b5
Axel Monsalve <axel.monsalve.ponce@gmail.com>
2020-08-26 12:44:03 -0400

    Mostrando el Nombre del Proyecto

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,35 +1,43 @@
-import React from 'react'
-import Tarea from './Tarea'
+import React, {useContext} from "react";
+import Tarea from "./Tarea";
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 function ListadoTareas() {
+  //Obtener el state del formulario
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  //Si no hay proyecto seleccionado
+  if(!proyecto){
+    return <h2>Selecciona un proyecto</h2>
+  }
+
+  //Extraer proyecto actual
+  const [proyectoActual] = proyecto
 
   const tareasProyecto = [
-    {nombre: 'Elegir plataforma', estado: true},
-    {nombre: 'Elegir colores', estado: false},
-    {nombre: 'Elegir hosting', estado: false},
-    {nombre: 'Elegir plataformas de pago', estado: true},
-  ]
+    { nombre: "Elegir plataforma", estado: true },
+    { nombre: "Elegir colores", estado: false },
+    { nombre: "Elegir hosting", estado: false },
+    { nombre: "Elegir plataformas de pago", estado: true },
+  ];
 
   return (
     <>
-      <h2>Proyecto: Tienda Virtual</h2>
+      <h2>Proyecto: {proyectoActual.nombre}</h2>
       <ul className="listado-tareas">
         {tareasProyecto.length === 0 ? (
-          <li className="tarea"><p>No hay tareas</p></li>
-          ) : 
-          tareasProyecto.map(tarea =>(
-            <Tarea
-              tarea={tarea}
-            />
-            ))
-        }
+          <li className="tarea">
+            <p>No hay tareas</p>
+          </li>
+        ) : (
+          tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
+        )}
       </ul>
 
-      <button
-      className="btn btn-eliminar"
-      >Eliminar Proyecto &times;</button>
+      <button className="btn btn-eliminar">Eliminar Proyecto &times;</button>
     </>
-  )
+  );
 }
 
-export default ListadoTareas
+export default ListadoTareas;
