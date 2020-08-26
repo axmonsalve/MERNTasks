df9c2006b67e22329bbb3ae5fae58a8499ec6cab
Axel Monsalve <axel.monsalve.ponce@gmail.com>
2020-08-26 12:48:07 -0400

    Ocultando el Formulario de Tareas si no hay Proyecto Seleccionado

--- src/components/proyectos/Proyecto.js
+++ src/components/proyectos/Proyecto.js
@@ -1,21 +1,21 @@
-import React, {useContext} from 'react'
+import React, { useContext } from "react";
 import proyectoContext from "../../context/proyectos/proyectoContext";
 
-function Proyecto({proyecto}) {
+function Proyecto({ proyecto }) {
   //Obtener el state de proyectos
   const proyectosContext = useContext(proyectoContext);
-  const {
-    proyectoActual
-  } = proyectosContext;
+  const { proyectoActual } = proyectosContext;
   return (
     <li>
-        <button
-          type="button"
-          className="btn btn-blank"
-          onClick={()=> proyectoActual(proyecto.id)}
-        >{proyecto.nombre}</button>
+      <button
+        type="button"
+        className="btn btn-blank"
+        onClick={() => proyectoActual(proyecto.id)}
+      >
+        {proyecto.nombre}
+      </button>
     </li>
-  )
+  );
 }
 
-export default Proyecto
+export default Proyecto;
--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 function FormTarea() {
+  //Extraer si un proyecto esta activo
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  //Si no hay proyecto seleccionado
+  if(!proyecto){
+    return null
+  }
   return (
     <div className="formulario">
       <form
