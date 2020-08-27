7e74cd42ca7c01b0287fa57a20427711e3e2b221
Axel Monsalve <axel.monsalve.ponce@gmail.com>
2020-08-26 22:55:19 -0400

    Creando un dispatch para nuevas tareas

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -10,15 +10,33 @@ function FormTarea() {
   if(!proyecto){
     return null
   }
+
+  //Array destructuring para extraer el proyecto actual
+  const [proyectoActual] = proyecto
+
+  const onSubmit = e => {
+    e.preventDefault()
+
+    //Validar
+
+    //Pasar validacion
+
+    //Agregar la nueva tarea al state de tareas
+
+    //Reiniciar el form
+  }
+
+
   return (
     <div className="formulario">
       <form
+        onSubmit={onSubmit}
       >
         <div className="contenedor-input">
           <input type="text" className="input-text" name="nombre" placeholder="Nombre Tarea..."/>
         </div>
         <div className="contenedor-input">
-          <input type="submit" value="Agregar Tarea" className="btn btn-block btn-priirio"/>
+          <input type="submit" value="Agregar Tarea" className="btn btn-block btn-primario"/>
         </div>
       </form>
     </div>
