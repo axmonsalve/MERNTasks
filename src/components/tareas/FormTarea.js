81b08a12c2373bffdeabad5f0708eccaaa2c12cf
Axel Monsalve <axel.monsalve.ponce@gmail.com>
2020-08-26 23:26:38 -0400

    Insertando tareas en el Proyecto Actual

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,10 +1,22 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import proyectoContext from "../../context/proyectos/proyectoContext";
+import tareaContext from '../../context/tareas/tareaContext'
 
 function FormTarea() {
   //Extraer si un proyecto esta activo
   const proyectosContext = useContext(proyectoContext);
   const { proyecto } = proyectosContext;
+
+  const tareasContext = useContext(tareaContext);
+  const { agregarTarea } = tareasContext;
+
+  //state del formulario
+  const [tarea, setTarea] = useState({
+    nombre: ''
+  })
+
+  //Extraer el nombre del proyecto
+  const {nombre} = tarea
 
   //Si no hay proyecto seleccionado
   if(!proyecto){
@@ -14,6 +26,14 @@ function FormTarea() {
   //Array destructuring para extraer el proyecto actual
   const [proyectoActual] = proyecto
 
+  //Leer los valores del formulario
+  const handleChange = e => {
+    setTarea({
+      ...tarea,
+      [e.target.name] : e.target.value
+    })
+  }
+
   const onSubmit = e => {
     e.preventDefault()
 
@@ -22,6 +42,9 @@ function FormTarea() {
     //Pasar validacion
 
     //Agregar la nueva tarea al state de tareas
+    tarea.proyectoId = proyectoActual.id
+    tarea.estado = false
+    agregarTarea(tarea)
 
     //Reiniciar el form
   }
@@ -33,7 +56,7 @@ function FormTarea() {
         onSubmit={onSubmit}
       >
         <div className="contenedor-input">
-          <input type="text" className="input-text" name="nombre" placeholder="Nombre Tarea..."/>
+          <input type="text" className="input-text" name="nombre" value={nombre} placeholder="Nombre Tarea..." onChange={handleChange}/>
         </div>
         <div className="contenedor-input">
           <input type="submit" value="Agregar Tarea" className="btn btn-block btn-primario"/>
